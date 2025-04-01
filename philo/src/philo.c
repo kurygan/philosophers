d86aca639380a9be4cf22619228505cb9d4fed59
kurygan <medhy.kettab06@gmail.com>
2025-04-01 17:45:55 +0200

    initialize the global structure and protect main from wrong number of args

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -1,33 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.h                                            :+:      :+:    :+:   */
+/*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tylerlover911 <tylerlover911@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/01 17:35:51 by tylerlover9       #+#    #+#             */
-/*   Updated: 2025/04/01 17:40:32 by tylerlover9      ###   ########.fr       */
+/*   Created: 2025/04/01 17:35:47 by tylerlover9       #+#    #+#             */
+/*   Updated: 2025/04/01 17:45:04 by tylerlover9      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILO_H
-# define PHILO_H
+#include "../include/philo.h"
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
-# include <sys/time.h>
-# include <pthread.h>
-
-typedef struct	s_global
+int	main(int ac, char **av)
 {
-	bool			alive;
-	int				philo_numbers;
-	size_t			time_started;
-	pthread_mutex_t	lock_write;
-	pthread_mutex_t	lock_dead;
-	pthread_mutex_t	lock_meal;
-}	t_global;
+	t_global	dinner;
 
-#endif
+	if (ac != 5 || ac != 6)
+		return(fprintf(stderr, "Error: Number of Args"), 0);
+	memset(&dinner, 0, sizeof(t_global));
+}
